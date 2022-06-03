8848599ac1fbeeafbc4f166cd114acc33196ae6b
Priska-87 <prisciladk23@hotmail.com>
2022-06-02 21:16:22 -0300

    Se inicia proyecto con función suma

---
+++ practico_python.py
@@ -0,0 +1,11 @@
+"""Suma: Priscila Kwiatkowski Aula 3"""
+
+num1= int(input("Ingrese un valor numérico: "))
+num2= int(input("Ingrese un valor numérico: "))
+num3= int(input("Ingrese un valor numérico: "))
+
+def suma (num1, num2, num3):
+    resultado = num1 + num2 + num3
+    print(f"El resultado es:{resultado}")  
+
+suma(num1, num2, num3)

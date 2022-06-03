a5315e4e0a39f4d5d5978303c406adea4ee5c696
erics4com <64873482+erics4com@users.noreply.github.com>
2022-06-03 15:41:35 -0300

    practico_python_1
    multiplicacion agregada

--- practico_python_1.py
+++ practico_python_1.py
@@ -18,3 +18,15 @@ def resta(a,b):
     return (a - b)
 
 #print(resta(10,5))
+
+
+# producto : Eric diaz
+
+print ("¿ Ahora quieres intentar calcular el producto ? ")
+print ("ingresa dos números")
+
+numero_1 = int(input("Introduce tu primer número: "))
+numero_2 = int(input("Introduce tu segundo número: "))
+
+print("RESULTADO: El producto de",numero_1,"*",numero_2,"es igual a",numero_1*numero_2)
+

efcec9dcac06b1f113d6d9fdd5e263fb86b2973c
AaronC <terabitetiger@gmail.com>
2022-06-06 19:02:30 -0300

    Update practico_python_1.py

--- practico_python_1.py
+++ practico_python_1.py
@@ -1,15 +1,8 @@
 """Función Suma: Priscila Kwiatkowski Aula 3"""
 
-
-num1= int(input("Ingrese un valor numérico: "))
-num2= int(input("Ingrese un valor numérico: "))
-num3= int(input("Ingrese un valor numérico: "))
-
 def suma (num1, num2, num3):
     resultado = num1 + num2 + num3
-    print(f"El resultado es:{resultado}")  
-
-suma(num1, num2, num3)
+    return (resultado) 
 
 
 #Función Resta(): Aaron Cortez 
@@ -17,4 +10,56 @@ suma(num1, num2, num3)
 def resta(a,b):
     return (a - b)
 
+<<<<<<< Updated upstream
 #print(resta(10,5))
+=======
+
+# producto : Eric diaz
+
+def producto(numero_1,numero_2):
+    return (numero_1*numero_2)
+
+#imprimir : Aaron Cortez
+def imprimir(texto,valor):
+    print(texto,valor)
+
+
+def programa():
+    operacion = int(input("¿Que operacion desea realizar?\n1: Suma\n2: Resta\n3: Multiplicación\n"))
+    if operacion == 1:
+        num1= int(input("Ingrese un valor numérico: "))
+        num2= int(input("Ingrese un valor numérico: "))
+        num3= int(input("Ingrese un valor numérico: "))
+
+        imprimir("el resultado es:",suma(num1,num2,num3))
+
+    elif operacion == 2:
+        a= int(input("Ingrese el primer numero: "))
+        b= int(input("Ingrese el segundo numero: "))
+
+        imprimir("el resultado de la resta es: ", resta(a,b))
+
+    elif operacion == 3:
+        print ("ingresa dos números")
+        numero_1 = int(input("Introduce tu primer número: "))
+        numero_2 = int(input("Introduce tu segundo número: "))
+
+        mensaje = "RESULTADO: El producto de " + str(numero_1) + " * " + str(numero_2) + " es igual a: "
+
+        imprimir(mensaje,producto(numero_1,numero_2))
+    
+    else:
+        print("ingrese un valor valido: (1, 2 o 3)")
+        reiniciar()
+
+    nuevaOperacion = int(input("¿Desea realizar otra operación? :\n 1: Si\n 2: No\n"))
+
+    if nuevaOperacion == 1:
+        reiniciar()
+    elif nuevaOperacion == 2:
+        print("Usted a elegido No")
+def reiniciar():
+    programa()
+
+programa()
+>>>>>>> Stashed changes
